spdiswal/vitus

commit bc9d26ee4740c58feedca921aa7847d3220fe3e3

Improve HMR experience
Hot module replacement was not working well, often applying changes to
a few places only and forgetting all changes upon refreshing the page.

Rendering everything client-side seems to fix this. It's not perfect,
but it's a lot better.

src/VitusCli.ts
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 
+import { renderBodyHtml } from "+explorer/ExplorerServer"
+import { getInitialState } from "+explorer/state/ExplorerState"
 import { createEventStream } from "+server/EventStream"
 import { createEventStreamReporter } from "+server/EventStreamReporter"
-import { loadIndexHtml } from "+server/LoadIndexHtml"
 import {
 	handleEventStreamRequests,
 	handleIndexHtmlRequests,
+	loadIndexHtml,
 } from "+server/RequestHandlers"
 import polka from "polka"
 import sirv from "sirv"
@@ -14,17 +16,27 @@ import { startVitest } from "vitest/node"
 const port = 17280
 const base = "/"
 
-const cachedIndexHtml = loadIndexHtml("./dist/explorer/index.html")
+const deferredIndexHtml = loadIndexHtml("./dist/explorer/index.html")
 
 const eventStream = createEventStream()
-const vitest = await startVitest("test", [], {
+const deferredVitest = startVitest("test", [], {
 	reporters: [createEventStreamReporter(eventStream)],
 })
+
+const [indexHtml, vitest] = await Promise.all([
+	deferredIndexHtml,
+	deferredVitest,
+])
 
 polka()
 	.use(base, sirv("./dist/explorer/", { extensions: [] }))
 	.get("/api/events", handleEventStreamRequests(eventStream))
-	.get("*", handleIndexHtmlRequests(base, vitest, cachedIndexHtml))
+	.get(
+		"*",
+		handleIndexHtmlRequests(base, indexHtml, (requestUrl) =>
+			renderBodyHtml(getInitialState(vitest), requestUrl),
+		),
+	)
 	.listen(port, (): void => {
 		console.log(`Vitus is running at http://localhost:${port}`)
 	})

src/VitusDev.ts
@@ -1,9 +1,9 @@
 import { createEventStream } from "+server/EventStream"
 import { createEventStreamReporter } from "+server/EventStreamReporter"
-import { loadIndexHtml } from "+server/LoadIndexHtml"
 import {
 	handleEventStreamRequests,
 	handleIndexHtmlRequests,
+	loadIndexHtml,
 } from "+server/RequestHandlers"
 import polka from "polka"
 import { createServer } from "vite"
@@ -12,16 +12,24 @@ import { startVitest } from "vitest/node"
 const port = 8000
 const base = "/"
 
-const vite = await createServer({
+const deferredIndexHtml = loadIndexHtml("./src/index.html")
+
+const eventStream = createEventStream()
+const deferredVitest = startVitest("test", [], {
+	reporters: [createEventStreamReporter(eventStream)],
+})
+
+const deferredVite = createServer({
 	appType: "custom",
 	base,
 	server: { middlewareMode: true },
 })
 
-const eventStream = createEventStream()
-const vitest = await startVitest("test", [], {
-	reporters: [createEventStreamReporter(eventStream)],
-})
+const [indexHtml, , vite] = await Promise.all([
+	deferredIndexHtml,
+	deferredVitest,
+	deferredVite,
+])
 
 polka()
 	.use(vite.middlewares)
@@ -30,13 +38,9 @@ polka()
 		"*",
 		handleIndexHtmlRequests(
 			base,
-			vitest,
-			(requestUrl) =>
-				// Always load the most recent version of `index.html` on every request.
-				loadIndexHtml("./src/index.html", (html) =>
-					vite.transformIndexHtml(requestUrl, html),
-				),
-			(error) => vite.ssrFixStacktrace(error),
+			indexHtml,
+			async () => "", // Render everything client-side to prevent hydration errors that cause hot module replacement (HMR) to malfunction in Preact.
+			vite.ssrFixStacktrace,
 		),
 	)
 	.listen(port, (): void => {

src/server/RequestHandlers.ts
@@ -1,18 +1,16 @@
-import { renderBodyHtml } from "+explorer/ExplorerServer"
-import type { ExplorerState } from "+explorer/state/ExplorerState"
-import {
-	type FileTree,
-	mapModuleToFileTree,
-	mergeFileTrees,
-} from "+explorer/state/FileTree"
+import { readFile } from "node:fs/promises"
 import type { EventStream } from "+server/EventStream"
 import type { Event } from "+server/events/Event"
 import type { Middleware as PolkaMiddleware } from "polka"
-import type { Vitest } from "vitest/node"
 
 type RequestHandler = (request: Request, response: Response) => Promise<void>
 type Request = Parameters<PolkaMiddleware>[0]
 type Response = Parameters<PolkaMiddleware>[1]
+
+export async function loadIndexHtml(path: string): Promise<string> {
+	const indexHtml = await readFile(path, "utf-8")
+	return indexHtml.slice(0, -"</body></html>".length)
+}
 
 export function handleEventStreamRequests(
 	eventStream: EventStream,
@@ -49,25 +47,16 @@ export function handleEventStreamRequests(
 
 export function handleIndexHtmlRequests(
 	base: string,
-	vitest: Vitest,
-	getIndexHtmlHeader:
-		| Promise<string>
-		| ((requestUrl: string) => Promise<string>),
+	indexHtml: string,
+	renderHtml: (requestUrl: string) => Promise<string>,
 	onError?: (error: Error) => void,
 ): RequestHandler {
 	return async (request, response): Promise<void> => {
 		const requestUrl = request.originalUrl.replace(base, "")
 
-		const initialState = getInitialState(vitest)
-
 		try {
-			const [indexHtmlHeader, bodyHtml] = await Promise.all([
-				typeof getIndexHtmlHeader === "function"
-					? getIndexHtmlHeader(requestUrl)
-					: getIndexHtmlHeader,
-				renderBodyHtml(initialState, requestUrl),
-			])
-			const html = `${indexHtmlHeader}${bodyHtml}</body></html>`
+			const bodyHtml = await renderHtml(requestUrl)
+			const html = `${indexHtml}${bodyHtml}</body></html>`
 
 			response.writeHead(200, { "content-type": "text/html" }).end(html)
 		} catch (error) {
@@ -79,20 +68,4 @@ export function handleIndexHtmlRequests(
 			}
 		}
 	}
-}
-
-function getInitialState(vitest: Vitest): ExplorerState {
-	const modules = vitest.state.getTestModules()
-
-	const moduleStates = new Set(modules.map((module) => module.state()))
-	const status =
-		moduleStates.has("pending") || moduleStates.has("queued")
-			? "started"
-			: "completed"
-
-	const fileTree = modules
-		.map(mapModuleToFileTree)
-		.reduce<FileTree>(mergeFileTrees, [])
-
-	return { status, fileTree }
 }

src/explorer/state/ExplorerState.ts
@@ -1,6 +1,27 @@
-import type { FileTree } from "+explorer/state/FileTree"
+import {
+	type FileTree,
+	mapModuleToFileTree,
+	mergeFileTrees,
+} from "+explorer/state/FileTree"
+import type { Vitest } from "vitest/node"
 
 export type ExplorerState = {
 	status: "completed" | "disconnected" | "started"
 	fileTree: FileTree
+}
+
+export function getInitialState(vitest: Vitest): ExplorerState {
+	const modules = vitest.state.getTestModules()
+
+	const moduleStates = new Set(modules.map((module) => module.state()))
+	const status =
+		moduleStates.has("pending") || moduleStates.has("queued")
+			? "started"
+			: "completed"
+
+	const fileTree = modules
+		.map(mapModuleToFileTree)
+		.reduce<FileTree>(mergeFileTrees, [])
+
+	return { status, fileTree }
 }

src/server/LoadIndexHtml.ts
@@ -1,11 +1,0 @@
-import { readFile } from "node:fs/promises"
-
-export async function loadIndexHtml(
-	path: string,
-	transform?: (html: string) => Promise<string>,
-): Promise<string> {
-	const indexHtml = await readFile(path, "utf-8")
-	const transformedIndexHtml =
-		transform !== undefined ? await transform(indexHtml) : indexHtml
-	return transformedIndexHtml.slice(0, -"</body></html>".length)
-}

commit 7a1a55530edf7294846b120b2c92ba58ce7ceac6

Move the initial state to the `<head>` element
Commit bc9d26e accidentally deleted the initialisation script of the
state. This commit restores it and moves it to the `<head>` element.

src/VitusCli.ts
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
-import { renderBodyHtml } from "+explorer/ExplorerServer"
+import { renderBodyHtml, renderInitialState } from "+explorer/ExplorerServer"
 import { getInitialState } from "+explorer/state/ExplorerState"
 import { createEventStream } from "+server/EventStream"
 import { createEventStreamReporter } from "+server/EventStreamReporter"
 import {
 	handleEventStreamRequests,
 	handleIndexHtmlRequests,
-	loadIndexHtml,
+	loadIndexHtmlParts,
 } from "+server/RequestHandlers"
 import polka from "polka"
 import sirv from "sirv"
@@ -16,15 +16,15 @@ import { startVitest } from "vitest/node"
 const port = 17280
 const base = "/"
 
-const deferredIndexHtml = loadIndexHtml("./dist/explorer/index.html")
+const deferredIndexHtmlParts = loadIndexHtmlParts("./dist/explorer/index.html")
 
 const eventStream = createEventStream()
 const deferredVitest = startVitest("test", [], {
 	reporters: [createEventStreamReporter(eventStream)],
 })
 
-const [indexHtml, vitest] = await Promise.all([
-	deferredIndexHtml,
+const [indexHtmlParts, vitest] = await Promise.all([
+	deferredIndexHtmlParts,
 	deferredVitest,
 ])
 
@@ -33,9 +33,14 @@ polka()
 	.get("/api/events", handleEventStreamRequests(eventStream))
 	.get(
 		"*",
-		handleIndexHtmlRequests(base, indexHtml, (requestUrl) =>
-			renderBodyHtml(getInitialState(vitest), requestUrl),
-		),
+		handleIndexHtmlRequests(base, indexHtmlParts, (requestUrl) => {
+			const initialState = getInitialState(vitest)
+
+			return Promise.all([
+				renderInitialState(initialState),
+				renderBodyHtml(initialState, requestUrl),
+			])
+		}),
 	)
 	.listen(port, (): void => {
 		console.log(`Vitus is running at http://localhost:${port}`)

src/VitusDev.ts
@@ -1,9 +1,11 @@
+import { renderInitialState } from "+explorer/ExplorerServer"
+import { getInitialState } from "+explorer/state/ExplorerState"
 import { createEventStream } from "+server/EventStream"
 import { createEventStreamReporter } from "+server/EventStreamReporter"
 import {
 	handleEventStreamRequests,
 	handleIndexHtmlRequests,
-	loadIndexHtml,
+	loadIndexHtmlParts,
 } from "+server/RequestHandlers"
 import polka from "polka"
 import { createServer } from "vite"
@@ -12,7 +14,7 @@ import { startVitest } from "vitest/node"
 const port = 8000
 const base = "/"
 
-const deferredIndexHtml = loadIndexHtml("./src/index.html")
+const deferredIndexHtmlParts = loadIndexHtmlParts("./src/index.html")
 
 const eventStream = createEventStream()
 const deferredVitest = startVitest("test", [], {
@@ -25,8 +27,8 @@ const deferredVite = createServer({
 	server: { middlewareMode: true },
 })
 
-const [indexHtml, , vite] = await Promise.all([
-	deferredIndexHtml,
+const [indexHtmlParts, vitest, vite] = await Promise.all([
+	deferredIndexHtmlParts,
 	deferredVitest,
 	deferredVite,
 ])
@@ -38,8 +40,11 @@ polka()
 		"*",
 		handleIndexHtmlRequests(
 			base,
-			indexHtml,
-			async () => "", // Render everything client-side to prevent hydration errors that cause hot module replacement (HMR) to malfunction in Preact.
+			indexHtmlParts,
+			async () => [
+				renderInitialState(getInitialState(vitest)),
+				"", // Render the Preact app fully client-side to prevent hydration errors that cause hot module replacement (HMR) to malfunction.
+			],
 			vite.ssrFixStacktrace,
 		),
 	)

src/explorer/ExplorerServer.tsx
@@ -6,14 +6,12 @@ export async function renderBodyHtml(
 	initialState: ExplorerState,
 	_requestUrl: string,
 ): Promise<string> {
-	const deferredApp = renderToStringAsync(
+	return renderToStringAsync(
 		<ExplorerApp initialState={initialState} initialTheme={null} />,
 	)
-
-	return renderInitialStateProps(initialState) + (await deferredApp)
 }
 
-function renderInitialStateProps(initialState: ExplorerState): string {
+export function renderInitialState(initialState: ExplorerState): string {
 	// language=html
 	return `<script type="text/javascript">window.__VITUS_INITIAL_STATE__=${JSON.stringify(initialState)}</script>`
 }

src/index.html
@@ -16,9 +16,11 @@
 		document.documentElement.classList.toggle("dark", prefersDark)
 		window.__VITUS_INITIAL_THEME__ = selectedTheme
 	</script>
+	<!--head-->
 	<title>Vitest &ndash; Vitus</title>
 </head>
 <body class="bg-gray-50 text-gray-950 dark:bg-gray-950 dark:text-gray-50 text-base leading-none transition">
+	<!--body-->
 	<script type="module" src="/explorer/ExplorerClient.tsx"></script>
 	<noscript>You need to enable JavaScript to run this app.</noscript>
 </body>

src/server/RequestHandlers.ts
@@ -1,15 +1,20 @@
 import { readFile } from "node:fs/promises"
 import type { EventStream } from "+server/EventStream"
 import type { Event } from "+server/events/Event"
+import type { Vector } from "+types/Vector"
 import type { Middleware as PolkaMiddleware } from "polka"
 
 type RequestHandler = (request: Request, response: Response) => Promise<void>
 type Request = Parameters<PolkaMiddleware>[0]
 type Response = Parameters<PolkaMiddleware>[1]
 
-export async function loadIndexHtml(path: string): Promise<string> {
+const indexHtmlPartitioner = /<!--head-->|<!--body-->/
+
+export async function loadIndexHtmlParts(
+	path: string,
+): Promise<Vector<string, 3>> {
 	const indexHtml = await readFile(path, "utf-8")
-	return indexHtml.slice(0, -"</body></html>".length)
+	return indexHtml.split(indexHtmlPartitioner) as Vector<string, 3>
 }
 
 export function handleEventStreamRequests(
@@ -47,16 +52,16 @@ export function handleEventStreamRequests(
 
 export function handleIndexHtmlRequests(
 	base: string,
-	indexHtml: string,
-	renderHtml: (requestUrl: string) => Promise<string>,
+	indexHtmlParts: Vector<string, 3>,
+	renderHtmlOutlets: (requestUrl: string) => Promise<Vector<string, 2>>,
 	onError?: (error: Error) => void,
 ): RequestHandler {
 	return async (request, response): Promise<void> => {
 		const requestUrl = request.originalUrl.replace(base, "")
 
 		try {
-			const bodyHtml = await renderHtml(requestUrl)
-			const html = `${indexHtml}${bodyHtml}</body></html>`
+			const [headHtml, bodyHtml] = await renderHtmlOutlets(requestUrl)
+			const html = `${indexHtmlParts[0]}${headHtml}${indexHtmlParts[1]}${bodyHtml}${indexHtmlParts[2]}`
 
 			response.writeHead(200, { "content-type": "text/html" }).end(html)
 		} catch (error) {

src/types/Vector.ts
@@ -1,0 +1,19 @@
+/**
+ * A tuple type of `Item` repeated `Size` times, where `Size` is a non-negative integer.
+ * If `Size` is a union of non-negative integers, it produces a corresponding union of tuples.
+ *
+ * @see https://github.com/microsoft/TypeScript/issues/26223#issuecomment-674500430
+ */
+export type Vector<Item, Size extends number> = Size extends Size
+	? number extends Size
+		? Array<Item>
+		: AppendItemToTuple<Item, Size, []>
+	: never
+
+type AppendItemToTuple<
+	Item,
+	Size extends number,
+	Accumulator extends Array<unknown>,
+> = Accumulator["length"] extends Size
+	? Accumulator
+	: AppendItemToTuple<Item, Size, [Item, ...Accumulator]>

vite.config.ts
@@ -98,7 +98,7 @@ function minifyIndexHtmlPlugin(): VitePlugin {
 				collapseWhitespace: true,
 				minifyCSS: true,
 				minifyJS: { ecma: 2023, toplevel: true },
-				removeComments: true,
+				removeComments: false, // Preserve the outlet comments.
 				sortAttributes: true,
 				sortClassName: true,
 			}),